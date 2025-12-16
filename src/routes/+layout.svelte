654f86c046e678890d8dcbe940367e2fd9dedaf7
Tanish <tkhadse2@illinois.edu>
2025-12-16 16:32:44 -0600

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,20 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+    <title>Portfolio</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Google+Sans+Code:ital,wght@0,300..800;1,300..800&family=Instrument+Sans:ital,wght@0,400..700;1,400..700&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+
+</svelte:head>
+
+<style>
+	:global(body) {
+		background-color: #333333;
+	}
+</style>
+
 {@render children()}
