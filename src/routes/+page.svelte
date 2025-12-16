d00cd698afcef1b316c4ccc1dcb2dda66d53a738
Tanish <tkhadse2@illinois.edu>
2025-12-16 16:35:49 -0600

    minor change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div class="text-white p-6 font-mono select-none">
     <h1>Welcome to SvelteKit</h1>
     <p class="">Visit 
-        <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> 
+        <a href="https://svelte.dev/docs/kit" class="transition hover:border-b">svelte.dev/docs/kit</a> 
         to read the documentation
     </p>
 </div>
