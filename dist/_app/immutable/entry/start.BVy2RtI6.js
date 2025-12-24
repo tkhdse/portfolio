9685bad1a033a8299ea5b14c25e7b91d424882ba
Tanish <tkhadse2@illinois.edu>
2025-12-24 15:41:22 -0600

    deploy

--- dist/_app/immutable/entry/start.BVy2RtI6.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/bpn8uO03.js";export{o as load_css,r as start};
--- svelte.config.js
+++ svelte.config.js
@@ -12,12 +12,10 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			// See below for an explanation of these options
-			config: undefined,
 			platformProxy: {
-				configPath: undefined,
-				environment: undefined,
-				persist: undefined
+				configPath: 'wrangler.toml',
+				environment: 'development',
+				persist: true
 			},
 			fallback: 'plaintext',
 			routes: {
