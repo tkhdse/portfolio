3852d04e454f744f9a72c740a1ce9bc8fe7fc978
Tanish <tkhadse2@illinois.edu>
2025-12-24 15:44:48 -0600

    deploy pretty please

--- svelte.config.js
+++ svelte.config.js
@@ -3,26 +3,10 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			platformProxy: {
-				configPath: 'wrangler.toml',
-				environment: 'development',
-				persist: true
-			},
-			fallback: 'plaintext',
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			}
-		})
+		adapter: adapter()
 	}
 };
 
